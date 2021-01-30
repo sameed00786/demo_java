b71f2201e4af4a84f45d1808a19eaf15e678dc36
sameed00786 <sameedsarim00786@gmail.com>
2021-01-30 19:15:20 +0530

    1st commit

---
+++ src/main/java/com/javainuse/SpringbootHellowolrdApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHellowolrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHellowolrdApplication.class, args);
+	}
+
+}
